66f4ac3af5516a9ad52439d94d25e3d04d8016e8
mirzoo <mirzo.okhunov@gmail.com>
2022-07-12 19:12:44 +0300

    add main

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    $('.header-burger').click(function(event) {
+        $('.header-burger,.header-menu').toggleClass('active');
+        $('body').toggleClass('lock')
+    });
+
+});
