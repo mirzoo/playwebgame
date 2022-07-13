ffaa16b1c75c7e73560e32ea9b5df23f28828cd7
mirzoo <mirzo.okhunov@gmail.com>
2022-07-13 04:43:10 +0300

    add support privacy

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ $(document).ready(function() {
     });
 
 });
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
